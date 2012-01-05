23c094ac4cdfce1e9605bd736fffbca6a13e940e
hide <hide@hide-linux.localhost.localdomain>
2012-01-05 18:44:12 +0900

    topbarにログイン認証フォームを移動。

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,25 @@
+# -*- coding: utf-8 -*-
+
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  # SSLアクセスのみを許可する場合は以下をコメントアウトする
+  # force_ssl
+
+  before_filter :login_required
+
+  helper_method :current_user
+
+  private
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def login_required
+    unless current_user
+      flash[:error] = "Login required"
+      redirect_to root_path
+    end
+  end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,19 @@
 class SessionsController < ApplicationController
+  skip_before_filter :login_required, only: [:create]
+
+  def create
+    user = User.find_by_login(params[:login])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to root_url, notice: "Logged in"
+    else
+      flash[:error] = "Invalid login or password"
+      render "new"
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to root_url, notice: "Logged out"
+  end
 end
