610f907fda3010039b61ee060e69f993f1c012ee
hide <hide@hide-linux.localhost.localdomain>
2012-01-05 14:06:29 +0900

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CarrotSns::Application.config.secret_token = '7937685afa93eae2e7070bc4a2533d5ae07586285f1d969b85a598e92f6a68759071ccfef6bcfd21f3eb37f7c95914403cce2e96ed0cfaa2d3de294eb7091568'
