687a1062e5f6627d81f6a011ba03084a7203ecd1
Hidekazu Nozaka <hide@netlab.jp>
2012-01-12 15:40:14 +0900

    ページネーションのデフォルト表示数を定義

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,4 @@ class User < ActiveRecord::Base
   has_secure_password
   validates :password, presence: true, on: :create
   validates :login, presence: true, uniqueness: true
-
-  paginates_per 5
 end
