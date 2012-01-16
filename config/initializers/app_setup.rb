e2e587f6f02e1bc6487fa8dc0741c604a748ab7a
Hidekazu Nozaka <hide@netlab.jp>
2012-01-16 11:58:44 +0900

    選択可能言語をapp_setupに追い出し。

--- config/initializers/app_setup.rb
+++ config/initializers/app_setup.rb
@@ -1,3 +1,12 @@
-# initialize App
+# -*- coding: utf-8 -*-
 
+# initialize App
 require 'carrot_sns'
+
+# Original Configurations
+module CarrotSns
+  class Application < Rails::Application
+    config.avairable_languages = [["日本語", "ja"],
+                                  ["English", "en"]]
+  end
+end
