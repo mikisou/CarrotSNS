097f67d763db38a59470951e278a0e8c50fc8577
Hidekazu Nozaka <hide@netlab.jp>
2012-03-14 18:58:54 +0900

    Railsバージョンを3.2.2に更新し、twitter-bootstrap-railsのバージョンを2.x系にアップデート。

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-CarrotSns::Application.config.secret_token = '7937685afa93eae2e7070bc4a2533d5ae07586285f1d969b85a598e92f6a68759071ccfef6bcfd21f3eb37f7c95914403cce2e96ed0cfaa2d3de294eb7091568'
+CarrotSns::Application.config.secret_token = '42555cc9651b2f127785d8f7419510c0fd5928f34b81c55f792f958bab384d93b26578d908c2c247846d97ef9521be5422ed76e858333925dc37959a0498c439'
